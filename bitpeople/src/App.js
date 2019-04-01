93424198b2eeec275f7a2831b3af7aec4737da9c
BIT Student <classroom@bgit.rs>
2019-04-01 18:04:49 +0200

    search not finished

--- bitpeople/src/App.js
+++ bitpeople/src/App.js
@@ -7,17 +7,33 @@ import PostGrid from './entities/PostGrid'
 import HeaderGrid from './entities/HeaderGrid'
 
 
+
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
       users: [],
-      isListView: localStorage.getItem('state') === null || undefined ? true : JSON.parse(localStorage.getItem('state'))
+      isListView: localStorage.getItem('state') === null || undefined ? true : JSON.parse(localStorage.getItem('state')),
+      inputValue: ''
     }
   }
 
 
+  handleChange = event => {
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+
+  displaySearch = (event) => {
+    let usersInput = this.state.users
+    usersInput = usersInput.map(user => {
+      return user.firstName
+    })
+    let search = this.state.inputValue
+
+  }
 
   handleSwitchViewClick = () => {
     this.setState((prevState) => {
@@ -43,9 +59,10 @@ class App extends Component {
 
   render() {
     return (
-
       <div className="App">
         {this.state.isListView ? <Header reload={this.refrashPage} switchView={this.handleSwitchViewClick} title="BIT People" /> : <HeaderGrid reload={this.refrashPage} switchView={this.handleSwitchViewClick} title="BIT People" />}
+        <i className="fa fa-search"></i>
+        <input type="text" value={this.state.inputValue} onChange={this.displaySearch} />
         {this.state.isListView ? <PostList people={this.state.users} isListView={this.state.isListView} /> : <PostGrid people={this.state.users} />}
         <Footer title="Copyright &copy;" year="2019" />
       </div>
