f64345f7d6a2bf9909c6c179519207af32710999
BIT Student <classroom@bgit.rs>
2019-04-02 12:24:52 +0200

    feature 8 done

--- bitpeople/src/App.js
+++ bitpeople/src/App.js
@@ -8,31 +8,27 @@ import HeaderGrid from './entities/HeaderGrid'
 
 
 
+
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      filteredUsers: [],
       users: [],
       isListView: localStorage.getItem('state') === null || undefined ? true : JSON.parse(localStorage.getItem('state')),
       inputValue: ''
     }
   }
 
+  componentDidMount() {
+    this.refreshPage()
+  }
 
   handleChange = event => {
     this.setState({
       inputValue: event.target.value
     })
-  }
-
-  displaySearch = (event) => {
-    let usersInput = this.state.users
-    usersInput = usersInput.map(user => {
-      return user.firstName
-    })
-    let search = this.state.inputValue
-
   }
 
   handleSwitchViewClick = () => {
@@ -44,26 +40,46 @@ class App extends Component {
     })
   }
 
-  refrashPage = () => {
+  onSearchChangeInput = (e) => {
+    const searchInput = e.target.value
+
+    let filterUsers = null
+
+
+    if (searchInput !== '') {
+      filterUsers = this.state.filteredUsers.filter((user) => {
+        return user.fullName.includes(searchInput)
+      })
+    } else {
+      filterUsers = this.state.users
+    }
+
+    this.setState({
+      filteredUsers: filterUsers,
+      inputValue: searchInput
+    })
+
+  }
+
+  refreshPage = () => {
     fetchUsers()
       .then(result => {
         this.setState({
+          filteredUsers: result,
           users: result
         })
       })
   }
 
-  componentDidMount() {
-    this.refrashPage()
-  }
-
   render() {
+
     return (
       <div className="App">
-        {this.state.isListView ? <Header reload={this.refrashPage} switchView={this.handleSwitchViewClick} title="BIT People" /> : <HeaderGrid reload={this.refrashPage} switchView={this.handleSwitchViewClick} title="BIT People" />}
-        <i className="fa fa-search"></i>
-        <input type="text" value={this.state.inputValue} onChange={this.displaySearch} />
-        {this.state.isListView ? <PostList people={this.state.users} isListView={this.state.isListView} /> : <PostGrid people={this.state.users} />}
+        {this.state.isListView ? <Header reload={this.refreshPage} switchView={this.handleSwitchViewClick} title="BIT People" /> : <HeaderGrid reload={this.refreshPage} switchView={this.handleSwitchViewClick} title="BIT People" />}
+
+        <input type="text" value={this.state.inputValue} onChange={this.onSearchChangeInput} />
+
+        {this.state.isListView ? <PostList people={this.state.filteredUsers} isListView={this.state.isListView} /> : <PostGrid people={this.state.filteredUsers} />}
         <Footer title="Copyright &copy;" year="2019" />
       </div>
     );
--- bitpeople/src/entities/User.js
+++ bitpeople/src/entities/User.js
@@ -2,6 +2,7 @@ class User {
     constructor(firstName, lastName, email, dateOfBirth, image, imageLarge, gender) {
         this.firstName = firstName
         this.lastName = lastName
+        this.fullName = `${firstName} ${lastName}`
         this.email = email.slice(0, 3) + '***' + email.slice(email.indexOf('@') - 3, email.indexOf('@')) + email.slice(email.indexOf('@'))
         this.dateOfBirth = dateOfBirth
         this.image = image
