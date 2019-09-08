a9d3808bd0b6cd3cb98cca58b55cbc441531530a
nijevise <calsoft@gmail.com>
2019-09-08 18:12:27 +0200

    first request start

--- food/src/App.js
+++ food/src/App.js
@@ -5,9 +5,30 @@ import RecipeDetails from './components/RecipeDetails';
 
 
 class App extends Component{
-  render(){
+  state = {
+    recipes : '',
+    url : "https://www.themealdb.com/api/json/v1/1/categories.php"
+  };
 
-  
+  async getRecipes(){
+    try{
+      const data = await fetch(this.state.url);
+      const jsonData = data.json();
+      this.setState({
+        recipes : jsonData.categories
+      })
+    }catch(error){
+      console.log(error)
+    }
+    
+  }
+  componentDidMount(){
+    this.getRecipes();
+  }
+
+
+  render(){
+      console.log(this.state.recipes);
   return (
     <React.Fragment>
       <RecipeList/>
